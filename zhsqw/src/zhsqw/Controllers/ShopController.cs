4332609e0f9003912b239ebcc1238016e87d933c
zhukuntang@163.com <zhukuntang@163.com>
2016-05-10 16:57:47 +0800

    start

---
+++ zhsqw/src/zhsqw/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using zhsqw.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Internal;
+
+namespace zhsqw.Controllers
+{
+    public class ShopController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("www", "zkt");
+            
+            return View(_context.wy_news.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Index(FormCollection collection)
+        {
+            //ViewBag.abc = collection["abc"];
+            ViewBag.abc2 = HttpContext.Session.GetString("www");
+            return View(_context.wy_news.ToList());
+        }
+    }
+}
---
+++ zhsqw/src/zhsqw/Models/wuye.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zhsqw.Models
+{
+    public class wy_news
+    {
+        public string ID { get; set; }
+        public string AreaID { get; set; }
+        public string NewsClass { get; set; }
+        public string NewsPic { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsContent { get; set; }
+    }
+
+    public class wy_ads
+    {
+        public string ID { get; set; }
+        public string AdOrder { get; set; }
+        public string AdPath { get; set; }
+    }
+}
